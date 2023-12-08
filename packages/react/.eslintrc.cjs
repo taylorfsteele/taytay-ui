4097674c72977f9f36741ad26f9db80675cd469e
tsteele <taylor.steele@disneystreaming.com>
2023-12-08 13:55:35 -0500

    ci: add linting

--- packages/react/.eslintrc.cjs
+++ packages/react/.eslintrc.cjs
@@ -1,11 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: [
-    "@taytay-ui/eslint-config/react-component-library.js",
-    "plugin:storybook/recommended"
-  ],
+  extends: ["@taytay-ui/eslint-config/react-component-library.js", "plugin:storybook/recommended"],
   parser: "@typescript-eslint/parser",
+  ignorePatterns: ["testSetup.ts", "storybook-static", "coverage"],
   parserOptions: {
     project: "./tsconfig.lint.json",
   },
