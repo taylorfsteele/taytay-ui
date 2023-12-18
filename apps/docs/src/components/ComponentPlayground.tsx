1677a83ce6d04e2d72022d2c3719928f4f0737ef
tsteele <taylor.steele@disneystreaming.com>
2023-12-18 13:08:26 -0500

    feat(docs): add radio to playground

--- apps/docs/src/components/ComponentPlayground.tsx
+++ apps/docs/src/components/ComponentPlayground.tsx
@@ -1,6 +1,18 @@
 import { Button, Component, type ButtonProps, type ComponentProps } from "@taytay-ui/react";
 import { useState, type ComponentType, type InputHTMLAttributes } from "react";
-import { playground, componentDemo, controls } from "./ComponentPlayground.module.css";
+import {
+  playground,
+  componentDemo,
+  controls,
+  title,
+  components,
+  switchComponent,
+  inputComponent,
+  radioComponent,
+  item,
+  indicator,
+} from "./ComponentPlayground.module.css";
+import * as RadioGroup from "@radix-ui/react-radio-group";
 
 /**
  * We have to use this pattern of making a singleton of all the components
@@ -30,31 +42,56 @@ export interface ComponentPlaygroundProps<
   component: T;
   propData?: {
     propName: K;
-    control?: "input" | "switch";
+    label?: string;
+    control?: "input" | "switch" | "radio";
     defaultValue: ComponentPropsType[T][K];
-    options?: ComponentPropsType[T][K][];
+    options?: ComponentPropsType[T][K][] | undefined;
   }[];
 }
 
-const Switch = ({
-  handleUpdateProps,
-  onChange = () => {},
-  ...props
-}: InputHTMLAttributes<HTMLInputElement> & { handleUpdateProps: (checked: boolean) => void }) => (
-  <input
-    type="checkbox"
-    {...props}
-    onChange={(e) => {
-      onChange(e);
-      handleUpdateProps(e.target.checked);
-    }}
-  />
+interface ControlProps {
+  label: string;
+  options?: string[];
+  defaultValue?: string;
+  onValueChange?: (value: string) => void;
+}
+
+const SwitchControl = ({ label, ...props }: InputHTMLAttributes<HTMLInputElement> & ControlProps) => (
+  <div className={switchComponent}>
+    <label>{label}</label>
+    <input type="checkbox" {...props} />
+  </div>
 );
 
-const controlComponents = {
-  input: "input",
-  switch: Switch,
-};
+const InputControl = ({ label, ...props }: InputHTMLAttributes<HTMLInputElement> & ControlProps) => (
+  <div className={inputComponent}>
+    <label>{label}</label>
+    <input {...props} />
+  </div>
+);
+
+const RadioControl = ({
+  label,
+  options = [],
+  defaultValue = "",
+  onValueChange = () => {},
+}: InputHTMLAttributes<HTMLInputElement> & ControlProps) => (
+  <div>
+    <span>{label}</span>
+    <RadioGroup.Root className={radioComponent} defaultValue={defaultValue} onValueChange={onValueChange}>
+      {options.map((option, index) => (
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <RadioGroup.Item className={item} value={option} id={`rg-${index}`}>
+            <RadioGroup.Indicator className={indicator} />
+          </RadioGroup.Item>
+          <label className="Label" htmlFor={`rg-${index}`}>
+            {option}
+          </label>
+        </div>
+      ))}
+    </RadioGroup.Root>
+  </div>
+);
 
 export const ComponentPlayground = <
   T extends keyof typeof allComponents,
@@ -64,13 +101,14 @@ export const ComponentPlayground = <
   propData = [],
 }: ComponentPlaygroundProps<T, K>) => {
   const Component = allComponents[component];
-  const allProps = propData.reduce(
-    (prevValue, currValue) => ({
-      ...prevValue,
-      [currValue.propName]: currValue.defaultValue,
-    }),
-    {} as React.ComponentProps<any>,
-  );
+  const allProps =
+    propData.reduce(
+      (prevValue, currValue) => ({
+        ...prevValue,
+        [currValue.propName]: currValue.defaultValue,
+      }),
+      {} as React.ComponentProps<any>,
+    ) ?? {};
 
   const [propsState, setPropsState] = useState(allProps);
 
@@ -79,29 +117,59 @@ export const ComponentPlayground = <
       <div className={componentDemo}>
         <Component {...propsState} />
       </div>
-      <div className={`${controls}`}>
-        <p>Playground</p>
+      <div className={controls}>
+        <p className={title}>Playground</p>
         <hr />
-        {propData.map((propConfig, index) => {
-          if (!propConfig.control) return null;
+        <div className={components}>
+          {propData.map((propConfig, index) => {
+            if (!propConfig.control) return null;
+            const label = propConfig.label ?? propConfig.propName.toString();
 
-          const ControlComponent = controlComponents[propConfig.control];
+            if (propConfig.control === "input")
+              return (
+                <InputControl
+                  key={index}
+                  label={label}
+                  onChange={(e) =>
+                    setPropsState((prev: Record<string, any>) => ({
+                      ...prev,
+                      [propConfig.propName]: e.target.value,
+                    }))
+                  }
+                />
+              );
 
-          return (
-            <>
-              <label>{propConfig.propName.toString()}</label>
-              <ControlComponent
-                key={index}
-                handleUpdateProps={(value) =>
-                  setPropsState((prev: Record<string, any>) => ({
-                    ...prev,
-                    [propConfig.propName]: value,
-                  }))
-                }
-              />
-            </>
-          );
-        })}
+            if (propConfig.control === "switch")
+              return (
+                <SwitchControl
+                  label={label}
+                  onChange={(e) =>
+                    setPropsState((prev: Record<string, any>) => ({
+                      ...prev,
+                      [propConfig.propName]: e.target.checked,
+                    }))
+                  }
+                />
+              );
+
+            if (propConfig.control === "radio")
+              return (
+                <RadioControl
+                  label={label}
+                  options={propConfig.options as string[]}
+                  defaultValue={propConfig?.defaultValue as string}
+                  onValueChange={(value) =>
+                    setPropsState((prev: Record<string, any>) => ({
+                      ...prev,
+                      [propConfig.propName]: value,
+                    }))
+                  }
+                />
+              );
+
+            return null;
+          })}
+        </div>
       </div>
     </div>
   );
--- packages/react/src/Button/Button.tsx
+++ packages/react/src/Button/Button.tsx
@@ -5,8 +5,13 @@ import { type ReactNode } from "react";
 export interface ButtonProps {
   disabled?: boolean;
   children: ReactNode;
+  type?: "button" | "submit" | "reset";
 }
 
-export const Button = ({ children, disabled }: ButtonProps) => {
-  return <button disabled={disabled}>{children}</button>;
+export const Button = ({ children, disabled, type }: ButtonProps) => {
+  return (
+    <button type={type} disabled={disabled}>
+      {children}
+    </button>
+  );
 };
